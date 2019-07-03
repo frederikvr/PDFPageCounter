d13b382a734745215dd6b56d50f52af71feb7cb0
Frederik Van Ruyskensvelde <frederik.vanruyskensvelde@docbyte.com>
2019-07-03 09:12:31 +0200

    refactoring dir search & updating errorhandling

--- PDFPageCounter/Program.cs
+++ PDFPageCounter/Program.cs
@@ -17,59 +17,52 @@ namespace PDFPageCounter
         static void Main(string[] args)
         {
             List<string> pdfs = new List<string>();
-            //local
-            //var directories = Directory.GetDirectories(@"E:\Customers\Fidea\PDFsTestDocuments");
-
-            //on server fidea
-            var directories = Directory.GetDirectories(@"D:\Docbyte\DocShifterTesting\PDFsTestDocuments");
-
-            foreach (var directory in directories)
-            {
-                var listOfPdfsInDirectory = Directory.GetFiles(directory, "*.pdf");
-                foreach (var pdf in listOfPdfsInDirectory)
-                {
-                    try
-                    {
-                        pdfs.Add(pdf);
-                    }
-                    catch (Exception ex)
-                    {
-
-                        log.Info("Exception " + ex + "caught");
-                    }
-                }
-            }
-
             string timestamp = DateTime.UtcNow.ToString("yyyyMMdd_HHmmss", CultureInfo.InvariantCulture);
 
+            //INPUT
+
+            //local
+            //var listOfPdfsInDirectory = Directory.GetFiles(@"E:\Customers\Fidea\PDFPageCounterTestDoc", "*.pdf", SearchOption.AllDirectories);
+
+            //fidea test
+            var listOfPdfsInDirectory = Directory.GetFiles(@"C:\CustomerTesting\Fidea\Exportfolders\folderToPageCount\", "*.pdf", SearchOption.AllDirectories);
+
+            //OUTPUT
             //output local
             //var outputDirectory = @"E:\Customers\Fidea\" + timestamp + "FideaPDFPagesCounted.csv";
 
-            //output server fidea
-            var outputDirectory = @"D:\Docbyte\DocShifterTesting\" + timestamp + "FideaPDFPagesCounted.csv";
+            //output QUA server fidea
+            //var outputDirectory = @"D:\Docbyte\DocShifterTesting\" + timestamp + "FideaPDFPagesCounted.csv";
+
+            //output TST server fidea
+            var outputDirectory = @"C:\CustomerTesting\Fidea\Exportfolders\" + timestamp + "FideaPDFPagesCounted.csv";
 
             using (var writer = new StreamWriter(outputDirectory))
             using (var csv = new CsvWriter(writer))
-                foreach (var pdf in pdfs)
+                foreach (var pdf in listOfPdfsInDirectory)
                 {
+                    int pos = pdf.LastIndexOf(@"\") + 1;
+                    string nameOfPDF = pdf.Substring(pos, pdf.Length - pos);
+
                     try
                     {
                         using (var document = PdfiumViewer.PdfDocument.Load(pdf))
                         {
-                            string path = pdf.ToString();
-                            int pos = path.LastIndexOf(@"\") + 1;
-                            string nameOfPDF = path.Substring(pos, path.Length - pos);
 
-                            csv.WriteRecord(new PDFRecord() { Name = nameOfPDF, AmountOfPages = document.PageCount });
+                            csv.WriteRecord(new PDFRecord()
+                            {
+                                Name = nameOfPDF,
+                                AmountOfPages = document.PageCount
+                            });
                             csv.NextRecord();
 
-                            log.Info(pdf.ToString() + " pageCount = " + document.PageCount);
+                            log.Info(pdf + " pageCount = " + document.PageCount);
                         }
                     }
                     catch (Exception ex)
                     {
-                        log.Info("Exception " + ex + "caught");
-                        csv.WriteRecord(new PDFRecord() { Name = ex.ToString(), AmountOfPages = 0 });
+                        log.Info("Exception " + ex + " caught");
+                        csv.WriteRecord(new PDFRecord() { Name = nameOfPDF, AmountOfPages = -1 });
                         csv.NextRecord();
                     }
                 }
